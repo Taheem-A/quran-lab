a49995aa3fca12506b5679de148be76d1eb48c00
Taheem Akter <taheem.akter@gmail.com>
2026-04-15 14:49:58 -0400

    Implemented the CORS module to allow API usage in the frontend. Also updated the naming that was used in the previous version of the Qur'an JSON.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/routes.ts';
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/quran', router);
--- apps/api/src/services/quranService.ts
+++ apps/api/src/services/quranService.ts
@@ -1,27 +1,27 @@
-import quran from '../data/quran.json';
+import quran from "../data/quran.json";
 
 export function getAllChapters() {
-	return quran;
+  return quran;
 }
 
 export function getChapterByNumber(chapterNumber: number) {
-	return quran.find((chapter) => chapter.id === chapterNumber);
+  return quran.find((chapter) => chapter.id === chapterNumber);
 }
 
 export function getVerseByNumber(chapterNumber: number, verseNumber: number) {
-	const chapter = getChapterByNumber(chapterNumber);
-	if (!chapter || !chapter.verses) return null;
-	return chapter.verses.find((verse) => verse.id === verseNumber);
+  const chapter = getChapterByNumber(chapterNumber);
+  if (!chapter || !chapter.verses) return null;
+  return chapter.verses.find((verse) => verse.id === verseNumber);
 }
 
 export function getRevelationLocation(chapterNumber: number) {
-	const chapter = getChapterByNumber(chapterNumber);
-	if (!chapter) return null;
-	return chapter.type;
+  const chapter = getChapterByNumber(chapterNumber);
+  if (!chapter) return null;
+  return chapter.type;
 }
 
 export function getVerseCount(chapterNumber: number) {
-	const chapter = getChapterByNumber(chapterNumber);
-	if (!chapter) return null;
-	return chapter.total_verses;
+  const chapter = getChapterByNumber(chapterNumber);
+  if (!chapter) return null;
+  return chapter.verseCount;
 }
